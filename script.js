245b4cef976a742a3f5105df8921629406ac1a08
jagadeeshwari <jagadha2007@gmail.com>
2026-06-09 22:05:36 +0530

    papu-birthday

---
+++ script.js
@@ -0,0 +1,120 @@
+const yesBtn=document.getElementById("yesBtn");
+
+yesBtn.addEventListener("click",()=>{
+
+document.getElementById("spidermanSection").style.display="block";
+
+document.getElementById("spiderman")
+.classList.add("swing");
+
+});
+document
+.getElementById("closeCard")
+.addEventListener("click",()=>{
+
+    document
+        .querySelector(".boy")
+        .classList.add("moveBoy");
+
+    document
+        .querySelector(".girl")
+        .classList.add("moveGirl");
+
+    setTimeout(()=>{
+        document.querySelector(".heart").style.opacity="1";
+    },3000);
+
+    setTimeout(()=>{
+        document.querySelector(".birthdayText").style.opacity="1";
+    },3500);
+
+});
+document.getElementById("openGift")
+.addEventListener("click",()=>{
+
+    document.getElementById("page2")
+    .scrollIntoView({
+        behavior:"smooth"
+    });
+
+});
+
+document.getElementById("nextBtn")
+.addEventListener("click",()=>{
+
+    document.getElementById("page3")
+    .scrollIntoView({
+        behavior:"smooth"
+    });
+
+});
+document.getElementById("openCard")
+.addEventListener("click",()=>{
+
+    document.getElementById("page3").style.display="none";
+
+    document.getElementById("page4").style.display="flex";
+
+});
+document.getElementById("openGift")
+.addEventListener("click",()=>{
+
+    document.getElementById("page1").style.display="none";
+    document.getElementById("page2").style.display="flex";
+
+});
+document.getElementById("nextBtn")
+.addEventListener("click",()=>{
+
+    document.getElementById("page2").style.display="none";
+    document.getElementById("page3").style.display="flex";
+
+});
+document.getElementById("closeCard")
+.addEventListener("click",()=>{
+
+    document.getElementById("page3").style.display="none";
+    document.getElementById("finalScene").style.display="block";
+
+    document.querySelector(".boy")
+    .classList.add("moveBoy");
+
+    document.querySelector(".girl")
+    .classList.add("moveGirl");
+
+    setTimeout(()=>{
+        document.querySelector(".heart").style.opacity="1";
+    },3000);
+
+    setTimeout(()=>{
+        document.querySelector(".birthdayText").style.opacity="1";
+    },3500);
+
+});
+document.getElementById("closeLetter")
+.addEventListener("click",()=>{
+
+    document.getElementById("page4").style.display="none";
+
+    document.getElementById("finalScene").style.display="block";
+
+    document.querySelector(".boy")
+        .classList.add("moveBoy");
+
+    document.querySelector(".girl")
+        .classList.add("moveGirl");
+
+    setTimeout(()=>{
+        document.querySelector(".heart").style.opacity="1";
+    },3000);
+
+    setTimeout(()=>{
+        document.querySelector(".birthdayText").style.opacity="1";
+    },3500);
+
+});
+const noBtn = document.getElementById("noBtn");
+
+noBtn.addEventListener("click", () => {
+    alert("❤️ Oii Pattu, only YES is allowed! Click Yes 😘");
+});
